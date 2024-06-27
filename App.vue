e4506bb35c75d95f110d94f8adbeb3a1d14d3616
minhtrees <markbuicadarnian@gmail.com>
2024-06-27 14:55:54 +0700

    Enhanced map functionality with marker removal, custom popups, and geolocation

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div id="map" style="height: 500px;"></div>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+export default {
+  name: 'App',
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const map = L.map('map').setView([51.505, -0.09], 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(map);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* Ensure you import Leaflet CSS */
+@import "~leaflet/dist/leaflet.css";
+</style>
